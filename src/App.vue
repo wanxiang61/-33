fd50bb9ad2ea4b4f1dfd57c986e871895c4ee0b0
sword <819835668@qq.com>
2022-09-06 18:12:37 +0800

    feat:登录页完成布局

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <span class="toutiao toutiao-gengduo"></span>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="login">登录</div>
+  <router-view></router-view>
   </div>
 </template>
 
@@ -15,7 +9,7 @@ export default {}
 </script>
 <style>
 .login{
-  width: 694px;
+  width: 700px;
   height: 88px;
   background-color:skyblue ;
 }
