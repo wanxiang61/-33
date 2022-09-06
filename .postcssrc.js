9f044917a470b7b4dfa170cabf95d5e9f8e38108
sword <819835668@qq.com>
2022-09-06 16:13:51 +0800

    feat:postcss配置完成

---
+++ .postcssrc.js
@@ -0,0 +1,10 @@
+const pxToRem = require('postcss-pxtorem')
+module.exports = {
+  plugins: [
+    pxToRem({
+      rootValue: ({ file }) => (/vant/.test(file) ? 37.5 : 75),
+
+      propList: ['*']
+    })
+  ]
+}
